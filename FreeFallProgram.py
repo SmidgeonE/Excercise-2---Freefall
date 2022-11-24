ffc0448e679dadb595486e6cd17c8c7d39629a3c
Smidgeon <finlaynugent@gmail.com>
2022-11-24 23:46:46 +0000

    Completed D

--- FreeFallProgram.py
+++ FreeFallProgram.py
@@ -14,19 +14,19 @@ def plotData(xData, yData, yLabel, xLabel='Time /s', colour='green'):
     plt.xlabel(xLabel)
 
 
-y_init = 1000  # Initial Height m
+y_init = 39045  # Initial Height m
 vy_init = 0  # Initial velocity in ms^-1
 g = 9.81  # Accel due to gravity ms^-2
-m = 73  # Mass of object in kg
+m = 73 + 80 # Mass of object in kg
 cd = 1.15 # Drag Coeff for a sky diver (Average)
 rho0 = 1.2 # Air density at ambient temp + pressure in kg m^-3
 h = 7640 # height at which rho = rho0 / e in m
 
 # Number of calculations in each part
-numOfPoints = 2000
+numOfPoints = 5000
 # Number of dts in the time array
 tStart = 0  # Starting time t
-tEnd = 200 # End time t
+tEnd = 700 # End time t
 
 # Taking Felix to be roughly the shape of an ellipse, where at largest area = 2pi*a*b
 # His height, given by https://bodysize.org/en/felix-baumgartner/ is:
@@ -34,19 +34,36 @@ a = 1.7
 # Estimated waist diameter, given by https://www.cdc.gov/nchs/fastats/mens-health.htm
 b = 0.996 / np.pi
 # So, his cross-sectional area is:
-area = np.pi * a * b
+area = np.pi * b * a * np.cos(np.deg2rad(70))
 # Constant drag factor, in kg m^-1
 kConstant = cd * rho0 * area / 2
 
+# gamma for air (spec heat cap. for constant pressure over that for constant volume)
+gamma = 1.4
+# Molar gas constant
+R = 8.314472
+# Molar mass of dry air (in kg mol^-1)
+molMass = 0.0289645
 
-def partA(terminateAtFloor=True, variableRho=False):
+
+def tempAtAltitude(altitude):
+    if altitude <= 11000:
+        return 288.0 - (0.0065*altitude)
+    elif altitude >= 25100:
+        return 141.3 + (0.0030*altitude)
+    else:
+        return 216.5
+
+def analyticalMethod(terminateAtFloor=True, variableRho=False, inMach=False):
     # Arrays holding time, displacement, and velocity in the y.
     global t
     global y
     global vy
+    global k
     t = np.linspace(tStart, tEnd, numOfPoints)
     y = np.zeros(numOfPoints)
     vy = np.zeros(numOfPoints)
+    mach = np.zeros(numOfPoints)
 
     # Applying initial value for y and vy:
     y[0] = y_init
@@ -58,6 +75,8 @@ def partA(terminateAtFloor=True, variableRho=False):
         # If Rho is said to be variable, it will calculate it:
         if variableRho:
             k = (cd * area * rho0 / 2) * np.exp(-y[i-1]/h)
+        if inMach:
+            mach[i] = np.sqrt(gamma*R*tempAtAltitude(y[i-1])/molMass)
 
         vy[i] = -np.sqrt(m * g / k) * np.tanh(np.sqrt(k * g / m) * t[i - 1])
         y[i] = y_init - (m / k) * np.log(np.cosh(np.sqrt(k * g / m) * t[i - 1]))
@@ -67,6 +86,7 @@ def partA(terminateAtFloor=True, variableRho=False):
             y = y[:i]
             vy = vy[:i]
             t = t[:i]
+            mach = mach[:i]
             break
 
     # Plotting
@@ -75,15 +95,20 @@ def partA(terminateAtFloor=True, variableRho=False):
     plt.title("Part A")
     plotData(t, y, 'Altitude /m', colour='red')
     plt.subplot(212)
-    plotData(t, vy, 'Velocity /ms^-1', colour='pink')
+    if inMach:
+        plotData(t[2:], vy[2:]/mach[2:], 'Velocity /Mach', colour='pink')
+    else:
+        plotData(t, vy, 'Velocity /ms^-1', colour='pink')
     plt.show()
 
 
-def partB(variableRho=False):
+def eulerMethod(variableRho=False, inMach=False):
     # Arrays holding time, displacement, and velocity in the y.
+
     t = np.zeros(numOfPoints)
     y = np.zeros(numOfPoints)
     vy = np.zeros(numOfPoints)
+    mach = np.zeros(numOfPoints)
 
     # Applying initial value for y and vy:
     y[0] = y_init
@@ -96,6 +121,8 @@ def partB(variableRho=False):
         # If Rho is said to be variable, it will calculate it:
         if variableRho:
             k = (cd * area * rho0 / 2) * np.exp(-y[i - 1] / h)
+        if inMach:
+            mach[i] = np.sqrt(gamma * R * tempAtAltitude(y[i - 1]) / molMass)
 
         vy[i] = vy[i-1] - dt*(g + (k / m) * np.abs(vy[i - 1]) * vy[i - 1])
         y[i] = y[i-1] + dt*(vy[i-1])
@@ -106,6 +133,7 @@ def partB(variableRho=False):
             y = y[:i]
             vy = vy[:i]
             t = t[:i]
+            mach = mach[:i]
             break
 
 
@@ -113,16 +141,13 @@ def partB(variableRho=False):
     plt.figure(2)
     plt.subplot(211)
     plt.title("Part B")
-    plotData(t, y, 'bAltitude /m', colour='red')
+    plotData(t, y, 'Altitude /m', colour='red')
     plt.subplot(212)
-    plotData(t, vy, 'Velocity /ms^-1', colour='pink')
+    if inMach:
+        plotData(t[2:], vy[2:]/mach[2:], 'Velocity /Mach', colour='pink')
+    else:
+        plotData(t, vy, 'Velocity /ms^-1', colour='pink')
     plt.show()
-
-
-
-
-
-
 
 # %%
 
@@ -134,21 +159,32 @@ while True:
     answer = input('\nPlease enter one of the following letters, or q to quit:\n --(a) : '
                    'Free fall with constant m, k, and g\n'
                    ' --(b) : Euler method for computing freefall\n'
-                   ' --(c) : Euler method or Analytical method with variable air density\n')
+                   ' --(c) : Euler method or Analytical method with variable air density\n'
+                   ' --(d) : Euler method or Analytical with variable air density and in mach\n')
 
     if answer == 'a':
         stopAtFloor = input("Do you want it to stop when it reaches the floor? Enter Y if so\n")
-        partA(stopAtFloor == 'Y')
+        analyticalMethod(stopAtFloor == 'Y')
 
     elif answer == 'b':
-        partB()
+        eulerMethod()
 
     elif answer == 'c':
         whichOne = input("Which of a or b would you like to see with variable air resistance?\n")
         if whichOne == 'a':
-            partA(True, True)
+            analyticalMethod(True, True)
         elif whichOne == 'b':
-            partB(True)
+            eulerMethod(True)
+        else:
+            print("That was not an option, returning to start\n\n")
+
+    elif answer == 'd':
+        whichOne = input("Which of a or b would you like to see with variable air resistance"
+                         "and in terms of mach? \n")
+        if whichOne == 'a':
+            analyticalMethod(True, True, True)
+        elif whichOne == 'b':
+            eulerMethod(True, True)
         else:
             print("That was not an option, returning to start\n\n")
 
