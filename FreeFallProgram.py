4ed33e0a98bd9a6b688315bdad735ab8d05ac993
Smidgeon <finlaynugent@gmail.com>
2022-11-26 23:15:26 +0000

    Added variable grav strenght

--- FreeFallProgram.py
+++ FreeFallProgram.py
@@ -23,7 +23,7 @@ rho0 = 1.2 # Air density at ambient temp + pressure in kg m^-3
 h = 7640 # height at which rho = rho0 / e in m
 
 # Number of calculations in each part
-numOfPoints = 5000
+numOfPoints = 2000
 # Number of dts in the time array
 tStart = 0  # Starting time t
 tEnd = 700 # End time t
@@ -36,7 +36,7 @@ b = 0.996 / np.pi
 # So, his cross-sectional area is:
 area = np.pi * b * a * np.cos(np.deg2rad(70))
 # Constant drag factor, in kg m^-1
-kConstant = cd * rho0 * area / 2
+kConstant = cd * rho0 * area * 10 / 2
 
 # gamma for air (spec heat cap. for constant pressure over that for constant volume)
 gamma = 1.4
@@ -46,6 +46,14 @@ R = 8.314472
 molMass = 0.0289645
 
 
+# Gravitational Constant
+G = 6.67E-11
+# Radius of Earth
+earthR = 6.371E6
+# Mass of Earth
+earthM = 5.972E24
+
+
 def tempAtAltitude(altitude):
     if altitude <= 11000:
         return 288.0 - (0.0065*altitude)
@@ -53,6 +61,12 @@ def tempAtAltitude(altitude):
         return 141.3 + (0.0030*altitude)
     else:
         return 216.5
+
+
+def gAtAltitude(altitude):
+    return G * earthM / np.square(altitude+earthR)
+
+
 
 def analyticalMethod(terminateAtFloor=True, variableRho=False, inMach=False):
     # Arrays holding time, displacement, and velocity in the y.
@@ -92,7 +106,7 @@ def analyticalMethod(terminateAtFloor=True, variableRho=False, inMach=False):
     # Plotting
     plt.figure(1)
     plt.subplot(211)
-    plt.title("Part A")
+    plt.title("Analytical Method")
     plotData(t, y, 'Altitude /m', colour='red')
     plt.subplot(212)
     if inMach:
@@ -100,9 +114,10 @@ def analyticalMethod(terminateAtFloor=True, variableRho=False, inMach=False):
     else:
         plotData(t, vy, 'Velocity /ms^-1', colour='pink')
     plt.show()
+    print("This hit the ground in", t[-1], "seconds")
 
 
-def eulerMethod(variableRho=False, inMach=False):
+def eulerMethod(variableRho=False, inMach=False, variableGrav=False):
     # Arrays holding time, displacement, and velocity in the y.
 
     t = np.zeros(numOfPoints)
@@ -115,6 +130,7 @@ def eulerMethod(variableRho=False, inMach=False):
     vy[0] = vy_init
 
     dt = (tEnd-tStart)/numOfPoints
+    print(dt)
 
     for i in range(1, numOfPoints):
         k = kConstant
@@ -124,7 +140,10 @@ def eulerMethod(variableRho=False, inMach=False):
         if inMach:
             mach[i] = np.sqrt(gamma * R * tempAtAltitude(y[i - 1]) / molMass)
 
-        vy[i] = vy[i-1] - dt*(g + (k / m) * np.abs(vy[i - 1]) * vy[i - 1])
+        if variableGrav:
+            vy[i] = vy[i - 1] - dt * (gAtAltitude(y[i-1]) + (k / m) * np.abs(vy[i - 1]) * vy[i - 1])
+        else:
+            vy[i] = vy[i - 1] - dt * (g + (k / m) * np.abs(vy[i - 1]) * vy[i - 1])
         y[i] = y[i-1] + dt*(vy[i-1])
         t[i] = t[i-1] + dt
 
@@ -140,7 +159,7 @@ def eulerMethod(variableRho=False, inMach=False):
     # Plotting
     plt.figure(2)
     plt.subplot(211)
-    plt.title("Part B")
+    plt.title("Euler Method")
     plotData(t, y, 'Altitude /m', colour='red')
     plt.subplot(212)
     if inMach:
@@ -148,6 +167,7 @@ def eulerMethod(variableRho=False, inMach=False):
     else:
         plotData(t, vy, 'Velocity /ms^-1', colour='pink')
     plt.show()
+    print("This hit the ground in", t[-1], "seconds")
 
 # %%
 
@@ -157,10 +177,11 @@ print('------\nWelcome to Exercise 2\n\nObjectives:\n'
 
 while True:
     answer = input('\nPlease enter one of the following letters, or q to quit:\n --(a) : '
-                   'Free fall with constant m, k, and g\n'
+                   'Analytical method with constant m, k, and g\n'
                    ' --(b) : Euler method for computing freefall\n'
                    ' --(c) : Euler method or Analytical method with variable air density\n'
-                   ' --(d) : Euler method or Analytical with variable air density and in mach\n')
+                   ' --(d) : Euler method or Analytical with variable air density and in mach\n'
+                   ' --(e) : Euler method with variable air density and gravity\n')
 
     if answer == 'a':
         stopAtFloor = input("Do you want it to stop when it reaches the floor? Enter Y if so\n")
@@ -170,7 +191,7 @@ while True:
         eulerMethod()
 
     elif answer == 'c':
-        whichOne = input("Which of a or b would you like to see with variable air resistance?\n")
+        whichOne = input("Would you like to see the analytical (a) or euler method(b)?\n")
         if whichOne == 'a':
             analyticalMethod(True, True)
         elif whichOne == 'b':
@@ -179,14 +200,16 @@ while True:
             print("That was not an option, returning to start\n\n")
 
     elif answer == 'd':
-        whichOne = input("Which of a or b would you like to see with variable air resistance"
-                         "and in terms of mach? \n")
+        whichOne = input("Would you like to see the analytical (a) or euler method(b)?\n")
         if whichOne == 'a':
             analyticalMethod(True, True, True)
         elif whichOne == 'b':
             eulerMethod(True, True)
         else:
             print("That was not an option, returning to start\n\n")
+
+    elif answer == 'e':
+        eulerMethod(True, True, True)
 
     else:
         print("Thank you")
